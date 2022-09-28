d975d2253af9cb359576310cd8e02db515956f47
Bae Hoseog <qoghtjr86@gmail.com>
2022-09-28 11:52:41 +0900

    Commit 8

---
+++ OOP_Project_02.cpp
@@ -0,0 +1,154 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+enum {ERROR, CREATE, DEPOSIT, WITHDRAW, SHOWINFO, EXIT};
+
+class Account
+{
+private:
+    int acc_ID;
+    char* Name;
+    int Charges;
+    size_t len;
+
+public:
+    int count;
+    Account()
+    : acc_ID(0), Name(NULL), Charges(0), len(0), count(0)
+    {
+    }
+    
+    void ShowMenu()
+    {
+        cout<<"-----Menu------"<<endl;
+        cout<<"1. 계좌개설"<<endl;
+        cout<<"2. 입 금"<<endl;
+        cout<<"3. 출 금"<<endl;
+        cout<<"4. 계좌정보 전체 출력"<<endl;
+        cout<<"5. 프로그램 종료"<<endl;
+    }
+    
+    void Create(Account* arr)
+    {
+        char name=NULL;
+        cout<<"[계좌개설]"<<endl;
+        cout<<"계좌ID: ";cin>>acc_ID;
+        cout<<"이 름: ";cin>>name;
+        char* pstr = &name;
+        len=strlen(pstr)+1;
+        Name=new char[len];
+        strcpy(Name, pstr);
+        cout<<"입금액: ";cin>>Charges;
+        cout<<"입금완료"<<endl;
+        arr[count]=*this;
+        count++;
+    }
+    
+    void Deposit(Account* arr)
+    {
+        int ID;
+        cout<<"계좌ID: ";cin>>ID;
+        for(int i=0; i<10; i++)
+            if(arr[i].acc_ID == ID)
+            {
+                int money;
+                cout<<"입금액: ";cin>>money;
+                arr[i].Charges += money;
+                cout<<"입금완료"<<endl;
+                break;
+            }
+            else
+            {
+                continue;
+            }
+    }
+    
+    void WithDraw(Account* arr)
+    {
+        int ID;
+        cout<<"계좌ID: ";cin>>ID;
+        for(int i=0; i<10; i++)
+            if(arr[i].acc_ID == ID)
+            {
+                int money;
+                cout<<"출금액: ";cin>>money;
+                arr[i].Charges -= money;
+                cout<<"출금완료"<<endl;
+                break;
+            }
+            else
+            {
+                continue;
+            }
+    }
+  
+    void ShowInfo(Account* arr)
+    {
+        cout<<endl<<"[전체 계좌정보]"<<endl;
+        for(int i=0; i<10; i++)
+            if(arr[i].acc_ID != 0)
+            {
+                cout<<"계좌ID: "<<arr[i].acc_ID<<endl;
+                cout<<"이 름: "<<arr[i].Name<<endl;
+                cout<<"잔 액: "<<arr[i].Charges<<endl;
+                cout<<endl;
+            }
+            else
+                break;
+    }
+    
+    void Exit()
+    {
+        cout<<"프로그램을 종료합니다."<<endl;
+    }
+
+    void Error()
+    {
+        cout<<"잘못 입력하셨습니다."<<endl;
+    }
+};
+
+int main()
+{
+    Account* arr[10];
+    int select=0;
+    while(select!=5)
+    {
+        Account a;
+        a.ShowMenu();
+        cout<<"선택: ";cin>>select;
+        switch (select) {
+            case CREATE:
+            {
+                a.Create(*arr);
+                break;
+            }
+            case DEPOSIT:
+            {
+                a.Deposit(*arr);
+                break;
+            }
+            case WITHDRAW:
+            {
+                a.WithDraw(*arr);
+                break;
+            }
+            case SHOWINFO:
+            {
+                a.ShowInfo(*arr);
+                break;
+            }
+            case EXIT:
+            {
+                a.Exit();
+                break;
+            }
+            default:
+            {
+                a.Error();
+                break;
+            }
+        }
+    }
+}
