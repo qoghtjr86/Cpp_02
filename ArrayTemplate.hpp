db4d752b5076ddd13a5cece3613c2470fb1757d9
Bae Hoseog <qoghtjr86@gmail.com>
2022-11-08 12:11:46 +0900

    Commit 19

--- ArrayTemplate.hpp
+++ ArrayTemplate.hpp
@@ -5,43 +5,32 @@
 #include <cstdlib>
 using namespace std;
 
-template <typename P>
-class BoundChechPointPtrArray
+template <typename T>
+class BoundCheckArray
 {
 private:
-    P* arr;
+    T* arr;
     int arrlen;
     
-    BoundChechPointPtrArray(const BoundChechPointPtrArray& arr) { }
-    BoundChechPointPtrArray& operator=(const BoundChechPointPtrArray& arr) { return *this; }
+    BoundCheckArray(const BoundCheckArray& arr) { }
+    BoundCheckArray& operator=(const BoundCheckArray& arr) { }
     
 public:
-    BoundChechPointPtrArray(int len);
-    P& operator[] (int idx);
-    P operator[] (int idx) const;
+    BoundCheckArray(int len);
+    T& operator[] (int idx);
+    T operator[] (int idx) const;
     int GetArrLen() const;
-    ~BoundChechPointPtrArray();
+    ~BoundCheckArray();
 };
 
-template <typename P>
-BoundChechPointPtrArray<P>::BoundChechPointPtrArray(int len) :arrlen(len)
+template <typename T>
+BoundCheckArray<T>::BoundCheckArray(int len) :arrlen(len)
 {
-    arr=new P[len];
+    arr=new T[len];
 }
 
-template <typename P>
-P& BoundChechPointPtrArray<P>::operator[] (int idx)
-{
-    if(idx<0 || idx>=arrlen)
-    {
-        cout<<"Array index out of bound exception"<<endl;
-        exit(1);
-    }
-    return arr[idx];
-}
-
-template <typename P>
-P BoundChechPointPtrArray<P>::operator[] (int idx) const
+template <typename T>
+T& BoundCheckArray<T>::operator[] (int idx)
 {
     if(idx<0 || idx>=arrlen)
     {
@@ -51,10 +40,21 @@ P BoundChechPointPtrArray<P>::operator[] (int idx) const
     return arr[idx];
 }
 
-template <typename P>
-int BoundChechPointPtrArray<P>::GetArrLen() const { return arrlen; }
+template <typename T>
+T BoundCheckArray<T>::operator[] (int idx) const
+{
+    if(idx<0 || idx>=arrlen)
+    {
+        cout<<"Array index out of bound exception"<<endl;
+        exit(1);
+    }
+    return arr[idx];
+}
 
-template <typename P>
-BoundChechPointPtrArray<P>::~BoundChechPointPtrArray() { delete []arr; }
+template <typename T>
+int BoundCheckArray<T>::GetArrLen() const { return arrlen; }
+
+template <typename T>
+BoundCheckArray<T>::~BoundCheckArray() { delete []arr; }
 
 #endif /* ArrayTemplate_hpp */
--- PointTemplate.hpp
+++ PointTemplate.hpp
@@ -1,6 +1,9 @@
 #ifndef PointTemplate_hpp
 #define PointTemplate_hpp
 
+#include <iostream>
+using namespace std;
+
 template <typename T>
 class Point
 {
@@ -10,5 +13,15 @@ public:
     Point(T x=0, T y=0);
     void ShowPosition() const;
 };
+
+template <typename T>
+Point<T>::Point(T x, T y) : xpos(x), ypos(y)
+{ }
+
+template <typename T>
+void Point<T>::ShowPosition() const
+{
+    cout<<'['<<xpos<<", "<<ypos<<']'<<endl;
+}
 
 #endif /* PointTemplate_hpp */
