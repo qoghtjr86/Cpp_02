eb9688c0abc62f618f3ae60a235d390fe26f1b5f
Bae Hoseog <qoghtjr86@gmail.com>
2022-11-10 11:17:03 +0900

    Commit 21

--- DynamicCasting.cpp
+++ DynamicCasting.cpp
@@ -8,7 +8,7 @@ private:
 public:
     Car(int fuel) : fuelGauge(fuel)
     { }
-    void ShowCarState() { cout<<"잔여 연료량: "<<fuelGauge<<endl; }
+    virtual void ShowCarState() { cout<<"잔여 연료량: "<<fuelGauge<<endl; }
 };
 
 class Truck : public Car
@@ -19,7 +19,7 @@ public:
     Truck(int fuel, int weight)
         : Car(fuel), freightWeight(weight)
     { }
-    void ShowTruckState()
+    virtual void ShowTruckState()
     {
         ShowCarState();
         cout<<"화물의 무게: "<<freightWeight<<endl;
@@ -30,11 +30,13 @@ int main(void)
 {
     Car* pcar1=new Truck(80, 200);
     Truck* ptruck1=dynamic_cast<Truck*>(pcar1);
+    ptruck1->ShowTruckState();
     
     Car* pcar2=new Car(120);
     Truck* ptruck2=dynamic_cast<Truck*>(pcar2);
     
     Truck* ptruck3=new Truck(70, 150);
     Car* pcar3=dynamic_cast<Car*>(ptruck3);
+    pcar3->ShowCarState();
     return 0;
 }
